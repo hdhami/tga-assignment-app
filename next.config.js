e639addf0db99c34102d58f727b0928bdff8d280
hdham1 <hardhami@publicisgroupe.net>
2020-04-12 22:22:16 +0530

    used next fonts build instead of custom webpack

--- next.config.js
+++ next.config.js
@@ -1,32 +1,34 @@
 require('dotenv').config();
 
+const withFonts = require('next-fonts');
+
 const config = {
-    webpack: (config) => {
-        return {
-            ...config,
-            ...{
-                module: {
-                    rules: [
-                        ...config.module.rules,
-                        {
-                            test: /\.(woff2|woff|eot|svg|ttf)$/,
-                            use: [
-                                {
-                                    loader: 'file-loader',
-                                    options: {
-                                        name: 'static/fonts/[name].[ext]'
-                                    }
-                                }
-                            ]
-                        }
-                    ]
-                }
-            }
-        };
-    },
+    // webpack: (config) => {
+    //     return {
+    //         ...config,
+    //         ...{
+    //             module: {
+    //                 rules: [
+    //                     ...config.module.rules,
+    //                     {
+    //                         test: /\.(woff2|woff|eot|svg|ttf)$/,
+    //                         use: [
+    //                             {
+    //                                 loader: 'file-loader',
+    //                                 options: {
+    //                                     name: 'static/fonts/[name].[ext]'
+    //                                 }
+    //                             }
+    //                         ]
+    //                     }
+    //                 ]
+    //             }
+    //         }
+    //     };
+    // },
     publicRuntimeConfig: {
         FRONTPAGE_STORIES_API_ENDPOINT: process.env.FRONTPAGE_STORIES_API_ENDPOINT
     }
 };
 
-module.exports = config;
+module.exports = withFonts(config);
